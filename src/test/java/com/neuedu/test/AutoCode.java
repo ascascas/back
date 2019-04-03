b68f768efb2b3e942439d5f50ff878de41c92100
Your Name <you@example>
2019-04-03 10:37:28 +0800

    增加

---
+++ src/test/java/com/neuedu/test/AutoCode.java
@@ -0,0 +1,52 @@
+package com.neuedu.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2019/4/1.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class AutoCode {
+
+    public static void main(String[] args) {
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        //指定逆向工程配置文件
+        File configFile = new File(System.getProperty("user.dir") + "/src/test/resources/mybatis-generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,callback, warnings);
+            myBatisGenerator.generate(null);
+            System.out.println("生成完成");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
